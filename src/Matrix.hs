84716a9f5609995fee7b049a3075fafc6a2a2dd6
Viva-Lambda <76657254+Viva-Lambda@users.noreply.github.com>
2022-06-05 22:27:18 +0900

    matrix advancements

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -7,6 +7,7 @@ import Helper
 import BaseEnum
 
 import Data.Word
+import Data.List
 import Debug.Trace
 
 data Matrix = MList {mdata :: NonEmptyList Scalar,
@@ -55,3 +56,59 @@ mRowNb a =
         scalarCol = word2Scalar (mColNb a)
         scalarRow = (word2Scalar s) / scalarCol
     in scalar2Word scalarRow
+
+
+mgetColumn :: Matrix -> Word -> Vector
+mgetColumn mat cindex =
+    if cindex >= (mstride mat)
+    then let msg1 = "given column index is larger than stride " ++ show (mstride mat)
+             msg2 = msg1 ++ " index " ++ show cindex 
+         in traceStack msg2 zeroV 3
+    else let md = mdata mat
+             col_nb = word2Int $ mColNb mat
+             row_nb = word2Int $ mRowNb mat
+             cinx = word2Int cindex
+             cindices = [r * col_nb + cinx | r <- [0..row_nb]]
+             mapf index = getNL md index
+             (c:cs) = map mapf cindices
+         in fromList2Vec c cs
+
+mgetRow :: Matrix -> Word -> Vector
+mgetRow mat rindex =
+    let rnb = mRowNb mat
+    in if rindex >= rnb
+       then let msg1 = "given row index is larger than row number " ++ show rnb
+                msg2 = msg1 ++ " index " ++ show rindex 
+            in traceStack msg2 zeroV 3
+       else let md = mdata mat
+                col_nb = word2Int $ mColNb mat
+                rinx = word2Int rindex
+                rindices = [rinx * col_nb + c | c <- [0..col_nb]]
+                mapf index = getNL md index
+                (c:cs) = map mapf rindices
+            in fromList2Vec c cs
+
+mSetRow :: Matrix -> Word -> Vector -> Matrix
+mSetRow mat rowIndex newRow =
+    if rowIndex >= (mRowNb mat)
+    then traceStack "given row index is larger than number of rows of matrix" (mzero 1 1)
+    else let col_nb = word2Int $ mColNb mat
+             rinx = word2Int rowIndex
+             rindices = [(rinx * col_nb + c, c) | c <- [0..col_nb]]
+             md = mdata mat
+             getIf index = let (is_in, indx) = let (reals, cs) = unzip rindices
+                                                   val = elemIndex index reals
+                                               in case val of
+                                                    Just ind -> (True,
+                                                                 cs !! ind)
+                                                    Nothing -> (False, index) 
+                           in if is_in
+                              then vget newRow indx
+                              else getNL md indx
+             (c:cs) = map getIf [0..((lengthNL md) - 1)]
+         in MList {mdata = fromList2NL c cs, mstride = mstride mat}
+
+
+                               
+
+
